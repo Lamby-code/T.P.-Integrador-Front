55d8b1a0f94a80f03ff8bec9f897137257511791
Alan <139820659+Lamby-code@users.noreply.github.com>
2023-11-03 11:04:25 -0300

    ticket page added

---
+++ index.js
@@ -0,0 +1,15 @@
+let resumeBtn = document.getElementById("resumeButton");
+resumeBtn.addEventListener("click", showPrice);
+
+function showPrice() {
+    let communicationAlert = document.getElementById("payResume");
+    let discountSelect = document.getElementById("ticket-category");
+    let discount = discountSelect.value;
+    let amountInput = document.getElementById("ticket-amount");
+    let amount = amountInput.value;
+    let realPrice = amount*(200 - (200*discount));
+    communicationAlert.textContent = `Total a Pagar: $${realPrice}`;
+}
+
+
+
